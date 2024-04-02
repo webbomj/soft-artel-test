3390f245aae1d301de307a01aeb4a2ef85e30c35
webbomj <nethack@mail.ru>
2024-04-02 16:20:17 +0300

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideEffects(),
+    provideHttpClient(),
+  ],
+};
